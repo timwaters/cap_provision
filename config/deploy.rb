fd6e7d2029f176ee85cb4951f2f8081fa5b31c85
Tim Waters <chippy2005@gmail.com>
2013-01-17 15:44:44 +0000

    and again

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,3 +31,36 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+before "deploy", "deploy:create_release_dir"
+namespace :deploy do
+  task :create_release_dir, :except => {:no_release => true} do
+    run "mkdir -p #{fetch :releases_path}"
+  end
+end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(release_path, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install"
+
+    on_rollback do
+      if previous_release
+        run "cd #{previous_release} && bundle install"
+      else
+        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
+      end
+    end
+  end
+
+  task :bundle_new_release, :roles => :db do
+    bundler.create_symlink
+    bundler.install
+  end
+end
+before "deploy:assets:precompile", "bundler:bundle_new_release"
