97504a78f3f5a7dc782bac895c3e3325fb025fa8
Tim Waters <chippy2005@gmail.com>
2013-01-17 13:25:15 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapProvision::Application.config.secret_token = '2cd29f3fafa07d95ac8f73b13c73c740bc69bcfa20b1dabcd65498f17cd06fd89a240229dbc62d0c82e473451162d74fbf366706bc1bd7c001c5e746eb7ff4db'
